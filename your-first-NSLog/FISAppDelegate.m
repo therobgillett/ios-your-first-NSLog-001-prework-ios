4254076a65a45a9a3c876b675cba99bf62be9843
therobgillett <rob@wecliqapp.com>
2015-09-24 15:17:37 -0400

    Added NSLog Hello World

--- your-first-NSLog/FISAppDelegate.m
+++ your-first-NSLog/FISAppDelegate.m
@@ -5,12 +5,8 @@
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-
-    /**
     
-     * Hey! Write your code here! :D
-     
-     */
+    NSLog(@"Hello World");
     
     // do not alter
     return YES;  //
